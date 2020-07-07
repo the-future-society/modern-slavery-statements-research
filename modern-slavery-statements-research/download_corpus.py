5f8ba5ba54f4e579887cc1a4cc3169e171b06fac
Edgar Rootalu <edgar@bravetech.io>
2020-07-07 15:15:58 +0200

    Add the download script and tutorial.

---
+++ modern-slavery-statements-research/download_corpus.py
@@ -0,0 +1,132 @@
+import argparse
+import boto3
+from collections import defaultdict
+import pandas as pd
+from os import path, makedirs
+from tqdm import tqdm
+import re
+
+
+def get_aws_creds_from_session():
+    """
+    Gets AWS access credentials from the session to avoid having to print them in the console.
+    :return: str aws_access_key_id, aws_secret_access_key
+    """
+
+    session = boto3.Session()
+    credentials = session.get_credentials()
+
+    # Credentials are refreshable, so accessing your access key / secret key
+    # separately can lead to a race condition. Use this to get an actual matched
+    # set.
+    credentials = credentials.get_frozen_credentials()
+    key_id = credentials.access_key
+    access_key = credentials.secret_key
+
+    return key_id, access_key
+
+
+def get_bucket_client(key_id, access_key):
+    client = boto3.client('s3',
+         aws_access_key_id=key_id,
+         aws_secret_access_key=access_key)
+    return client
+
+def get_bucket_from_resource(bucket_name, key_id, access_key):
+    resource = boto3.resource('s3',
+         aws_access_key_id=key_id,
+         aws_secret_access_key=access_key)
+    return resource.Bucket(bucket_name)
+
+def get_first_folder_name(objects):
+    return f"{objects[0].split('/')[0]}/"
+
+def get_latest_folder_name(bucket_name, key_id, access_key):
+    """
+    Gets all objects in the given bucket 
+    Finds the latest ones
+    Returns the folder name of the latests objects
+
+    Args:
+        bucket_name (str): bucket name in which to download files
+
+    Returns:
+        str: latest modified folder name 
+    """
+    # client = get_bucket_client(key_id, access_key)
+    # objects = client.list_objects_v2(Bucket=bucket_name)['Contents']
+    # get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
+    #
+    # sorted_objects = [obj['Key'] for obj in sorted(objects, key=get_last_modified, reverse=True)]
+    # folder_name = get_first_folder_name(sorted_objects)
+
+
+    s3r = boto3.resource('s3',
+         aws_access_key_id=key_id,
+         aws_secret_access_key=access_key)
+    bucket = s3r.Bucket(bucket_name)
+    files_in_bucket = list(bucket.objects.all())
+    sorted_folder_names = sorted([re.sub(r'/.+', '', file.key) for file in files_in_bucket], reverse=True)
+    folder_name = get_first_folder_name(sorted_folder_names)
+    # latest_folder_name = sorted_folder_names[0]
+
+    if not path.exists(path.dirname(folder_name)):
+        makedirs(path.dirname(folder_name))
+    return folder_name
+
+def get_metadatas(obj):
+    """Searches for all metadatas given for one object
+
+    Args:
+        obj (ObjectSummary): one S3 object
+
+    Returns:
+       defaultdict : each metadata info is stored in this defaultdict
+    """
+    metadatas = defaultdict(str)
+    metadatas["bucket_name"] = obj.bucket_name
+    metadatas["filename"] = obj.key
+    metadatas["size"] = str(obj.size)
+    metadatas["last_modified"] = str(obj.last_modified)
+    metadatas["owner_name"] = str(obj.owner["DisplayName"])
+    metadatas["owner_id"] = str(obj.owner["ID"])
+    metadatas["storage_class"] = str(obj.storage_class)
+    return metadatas
+
+def download(bucket_name='modern-slavery-dataset-txt', metadata_path='./metadatas.csv', only_metadata=False, key_id=None, access_key=None):
+    """The main function of this file
+    Downloads the latest files modified in the given bucket
+    Writes a csv file containing all info from each downloaded file
+    Args: command line arguments
+    """
+
+    if (not key_id or not access_key):
+        key_id, access_key = get_aws_creds_from_session()
+
+    remote_directory = get_latest_folder_name(bucket_name, key_id, access_key)
+    bucket = get_bucket_from_resource(bucket_name, key_id, access_key)
+    metadatas = []
+    for obj in tqdm(bucket.objects.filter(Prefix = remote_directory)):
+        if only_metadata == False:
+            bucket.download_file(obj.key, obj.key)
+        metadatas.append(get_metadatas(obj))
+    df = pd.DataFrame(metadatas)
+    df.to_csv(metadata_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Dowloads the latest documents from AWS S3 ")
+    parser.add_argument('-i', type=str, help="Provides the aws access key id", required=False)
+    parser.add_argument('-a', type=str, help="Provides the aws secret access key", required=False)
+    parser.add_argument('-b', type=str, help="Provide filename to bucket name file,"
+                                             "default = modern-slavery-dataset-txt", 
+                        default="modern-slavery-dataset-txt", required=False)
+    parser.add_argument('-m', type=str, help="Provides metadata for each file outputed",
+                        default="./metadatas.csv", required=False)
+    parser.add_argument('--only_m', type=bool, help="Whether of not we want to output only metadatas" 
+                                                    "and not all files, default=false",
+                        default=False)
+    args = parser.parse_args()
+    
+    download(args.b, args.i, args.a, args.only_m, args.m)
+
