3649de9cd25d92b5ab251bf9ff8d6d4eb52e4603
Edgar Rootalu <edgar@bravetech.io>
2020-07-07 19:03:56 +0200

    Added logging.

--- modern_slavery_statements_research/download_corpus.py
+++ modern_slavery_statements_research/download_corpus.py
@@ -5,12 +5,23 @@ import pandas as pd
 from os import path, makedirs
 from tqdm import tqdm
 import re
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+# noinspection PyArgumentList
+logging.basicConfig(level=logging.INFO,
+                    format=log_fmt
+                    )
 
 
 def get_aws_creds_from_session():
     """
     Gets AWS access credentials from the session to avoid having to print them in the console.
-    :return: str aws_access_key_id, aws_secret_access_key
+    :return: str aws_access_aws_access_key_id, aws_secret_access_key
     """
 
     session = boto3.Session()
@@ -20,28 +31,28 @@ def get_aws_creds_from_session():
     # separately can lead to a race condition. Use this to get an actual matched
     # set.
     credentials = credentials.get_frozen_credentials()
-    key_id = credentials.access_key
-    access_key = credentials.secret_key
+    aws_access_key_id = credentials.access_key
+    aws_secret_access_key = credentials.secret_key
 
-    return key_id, access_key
+    return aws_access_key_id, aws_secret_access_key
 
 
-def get_bucket_client(key_id, access_key):
+def get_bucket_client(aws_access_key_id, aws_secret_access_key):
     client = boto3.client('s3',
-         aws_access_key_id=key_id,
-         aws_secret_access_key=access_key)
+         aws_access_key_id=aws_access_key_id,
+         aws_secret_access_key=aws_secret_access_key)
     return client
 
-def get_bucket_from_resource(bucket_name, key_id, access_key):
+def get_bucket_from_resource(bucket_name, aws_access_key_id, aws_secret_access_key):
     resource = boto3.resource('s3',
-         aws_access_key_id=key_id,
-         aws_secret_access_key=access_key)
+         aws_access_key_id=aws_access_key_id,
+         aws_secret_access_key=aws_secret_access_key)
     return resource.Bucket(bucket_name)
 
 def get_first_folder_name(objects):
     return f"{objects[0].split('/')[0]}/"
 
-def get_latest_folder_name(bucket_name, key_id, access_key):
+def get_latest_folder_name(bucket_name, aws_access_key_id, aws_secret_access_key):
     """
     Gets all objects in the given bucket 
     Finds the latest ones
@@ -53,7 +64,7 @@ def get_latest_folder_name(bucket_name, key_id, access_key):
     Returns:
         str: latest modified folder name 
     """
-    # client = get_bucket_client(key_id, access_key)
+    # client = get_bucket_client(aws_access_key_id, aws_secret_access_key)
     # objects = client.list_objects_v2(Bucket=bucket_name)['Contents']
     # get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
     #
@@ -62,16 +73,19 @@ def get_latest_folder_name(bucket_name, key_id, access_key):
 
 
     s3r = boto3.resource('s3',
-         aws_access_key_id=key_id,
-         aws_secret_access_key=access_key)
+         aws_access_key_id=aws_access_key_id,
+         aws_secret_access_key=aws_secret_access_key)
     bucket = s3r.Bucket(bucket_name)
+    logger.info("Identifying the most recent data folder...")
     files_in_bucket = list(bucket.objects.all())
     sorted_folder_names = sorted([re.sub(r'/.+', '', file.key) for file in files_in_bucket], reverse=True)
     folder_name = get_first_folder_name(sorted_folder_names)
     # latest_folder_name = sorted_folder_names[0]
 
+    logger.info(f"Storing local data in {folder_name}")
     if not path.exists(path.dirname(folder_name)):
         makedirs(path.dirname(folder_name))
+        # logger.info(f"Storing local data in {folder_name}")
     return folder_name
 
 def get_metadatas(obj):
@@ -93,28 +107,34 @@ def get_metadatas(obj):
     metadatas["storage_class"] = str(obj.storage_class)
     return metadatas
 
-def download(bucket_name='modern-slavery-dataset-txt', metadata_path='./metadatas.csv', only_metadata=False, key_id=None, access_key=None):
+def download(bucket_name='modern-slavery-dataset-txt', metadata_path='./metadatas.csv', only_metadata=False, aws_access_key_id=None, aws_secret_access_key=None):
     """The main function of this file
     Downloads the latest files modified in the given bucket
     Writes a csv file containing all info from each downloaded file
     Args: command line arguments
     """
 
-    if (not key_id or not access_key):
-        key_id, access_key = get_aws_creds_from_session()
 
-    remote_directory = get_latest_folder_name(bucket_name, key_id, access_key)
-    bucket = get_bucket_from_resource(bucket_name, key_id, access_key)
+    if (not aws_access_key_id or not aws_secret_access_key):
+        aws_access_key_id, aws_secret_access_key = get_aws_creds_from_session()
+
+    remote_directory = get_latest_folder_name(bucket_name, aws_access_key_id, aws_secret_access_key)
+    bucket = get_bucket_from_resource(bucket_name, aws_access_key_id, aws_secret_access_key)
     metadatas = []
     for obj in tqdm(bucket.objects.filter(Prefix = remote_directory)):
         if only_metadata == False:
             bucket.download_file(obj.key, obj.key)
         metadatas.append(get_metadatas(obj))
+
     df = pd.DataFrame(metadatas)
     df.to_csv(metadata_path)
 
+    logger.info("Download finished.")
+
 
 if __name__ == '__main__':
+
+
     parser = argparse.ArgumentParser(description="Dowloads the latest documents from AWS S3 ")
     parser.add_argument('-i', type=str, help="Provides the aws access key id", required=False)
     parser.add_argument('-a', type=str, help="Provides the aws secret access key", required=False)
@@ -126,7 +146,7 @@ if __name__ == '__main__':
     parser.add_argument('--only_m', type=bool, help="Whether of not we want to output only metadatas" 
                                                     "and not all files, default=false",
                         default=False)
-    args = parser.parse_args()
-    
-    download(args.b, args.i, args.a, args.only_m, args.m)
+    args = parser.parse_args('')
+
+    download(args.b, args.m, args.only_m, args.i, args.a)
 
