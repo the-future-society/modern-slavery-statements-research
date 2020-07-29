378cecfe65a1a791b1b6b66e28cdac7ab5d21835
Edgar Rootalu <edgar@bravetech.io>
2020-07-29 15:32:35 +0200

    eliminated the test data download.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/the-future-society/modern-slavery-statements-research/
 EMAIL = 'edgar@bravetech.io'
 AUTHOR = 'The Future Society'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
